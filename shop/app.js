66a7b6f6a204500c3ce07529d0f49f28a97c282f
nathalie <nathalie1226@hotmail.com>
2016-07-19 21:51:42 +0300

    trying to make the description page work

--- shop/app.js
+++ shop/app.js
@@ -1,26 +1,25 @@
-/**
- * Created by itc_user on 7/17/2016.
- */
+
 (function () {
+
     'use strict';
-    var app = angular.module('myApp',['ngRoute'])
+    angular.module('myApp',[]);
+    
+        // .config(function ($routeProvider) {
+        //     $routeProvider
+        //         .when('/', {
+        //             templateUrl: 'shop.html',
+        //             controller: 'StoreController'
+        //         })
+        //         .when('/description', {
+        //             templateUrl: 'partials/description.html',
+        //             controller: 'StoreController'
+        //         })
+        //         .when('/cart', {
+        //             templateUrl: 'partials/cart.html',
+        //             controller: 'StoreController'
+        //         })
+        //         .otherwise({redirectTo: '/'});
+        // });
 
-            .config(function ($routeProvider) {
-                $routeProvider
-                    .when('/', {
-                        templateUrl: 'shop.html',
-                        controller: 'StoreController'
-                    })
-                    .when('/description', {
-                        templateUrl: './partials/description.html',
-                        controller: 'StoreController'
-                    })
-                    .when('/cart', {
-                        templateUrl: './partials/cart.html',
-                        controller: 'StoreController'
-                    })
-                    .otherwise({redirectTo: '/'});
-            });
 
-
-    })();
+})();
--- shop/controller/controller.js
+++ shop/controller/controller.js
@@ -1,99 +1,150 @@
 /**
  * Created by itc_user on 7/17/2016.
  */
-(function(){
-angular.module('myApp',[]).controller('StoreController',function($scope){
-    $scope.selectedProduct = 0;
-   $scope.products=[{name:'diapers',
-        price:'25',
-       showD:false,
-        description:'an amazing package of diapers for your baby',
-            stock:'3',
-        soldOut:false,
-            canPurchase:true,
-       addToCart:false,
-        image:{src:'./images/diapers.jpg'}},
-        {name:'baby bottle',
-            price:'30',
-            showD:false,
-            description:'bottle that your baby will love',
-            stock:'3',
-            soldOut:false,
-            canPurchase:true,
-            addToCart:false,
-            image:{src:'./images/bottle.jpg'}},
-        {name:'pacifier',
-            price:'5',
-            showD:false,
-            description:'must have',
-            stock:'0',
-            soldOut:true,
-            canPurchase:true,
-            addToCart:false,
-            image:{src:'./images/pacifier.jpg'}},
-       {name:'stroller',
-           price:'250',
-           showD:false,
-           description:'an amazing package of diapers for your baby',
-           stock:'3',
-           soldOut:false,
-           canPurchase:true,
-           addToCart:false,
-           image:{src:'./images/doona.jpg'}},
-       {name:'formula',
-           price:'15',
-           showD:false,
-           description:'best formula your baby will love it',
-           stock:'4',
-           soldOut:false,
-           canPurchase:true,
-           addToCart:false,
-           image:{src:'./images/formula.jpg'}},
-       {name:'bath',
-           price:'20',
-           showD:false,
-           description:'must have',
-           stock:'0',
-           soldOut:true,
-           canPurchase:true,
-           addToCart:false,
-           image:{src:'./images/bath.jpg'}},
-       {name:'carrier',
-           price:'100',
-           showD:false,
-           description:'an amazing package of diapers for your baby',
-           stock:'3',
-           soldOut:false,
-           canPurchase:true,
-           addToCart:false,
-           image:{src:'./images/baby-carrier.jpg'}},
-       {name:'crib',
-           price:'650',
-           showD:false,
-           description:'bottle that your baby will love',
-           stock:'3',
-           soldOut:false,
-           canPurchase:true,
-           addToCart:false,
-           image:{src:'./images/crib.jpg'}},
-       {name:'diaper-bag',
-           price:'40',
-           showD:false,
-           description:'must have',
-           stock:'0',
-           soldOut:true,
-           canPurchase:true,
-           addToCart:false,
-           image:{src:'./images/diaper-bag.jpg'}}
-    ];
+(function () {
+   var app=angular.module('myApp',[]);
+        // app.config(function ($routeProvider) {
+        //     $routeProvider
+        //         .when('/', {
+        //             templateUrl: 'shop.html',
+        //             controller: 'StoreController'
+        //         })
+        //         .when('/description', {
+        //             templateUrl: 'partials/description.html',
+        //             controller: 'StoreController'
+        //         })
+        //         .when('/cart', {
+        //             templateUrl: 'partials/cart.html',
+        //             controller: 'StoreController'
+        //         })
+        //         .otherwise({redirectTo: '/'});
+        // })
 
-    $scope.showDescription=function(){
-        $scope.selectedProduct = $scope.products.filter(function(item){
-            return !item.showD;
-        });
-        console.log( $scope.selectedProduct);
-        window.location = "./partials/description.html";
-    }
-    
+   app.controller('StoreController', function ($scope) {
+        $scope.selectedProduct = 0;
+        $scope.products = [{
+            name: 'diapers',
+            price: '25',
+            showD: true,
+            description: 'an amazing package of diapers for your baby',
+            stock: '3',
+            soldOut: false,
+            canPurchase: true,
+            addToCart: false,
+            image: {src: './images/diapers.jpg'}
+        },
+            {
+                name: 'baby bottle',
+                price: '30',
+                showD: false,
+                description: 'bottle that your baby will love',
+                stock: '3',
+                soldOut: false,
+                canPurchase: true,
+                addToCart: false,
+                image: {src: './images/bottle.jpg'}
+            },
+            {
+                name: 'pacifier',
+                price: '5',
+                showD: false,
+                description: 'must have',
+                stock: '0',
+                soldOut: true,
+                canPurchase: true,
+                addToCart: false,
+                image: {src: './images/pacifier.jpg'}
+            },
+            {
+                name: 'stroller',
+                price: '250',
+                showD: false,
+                description: 'an amazing package of diapers for your baby',
+                stock: '3',
+                soldOut: false,
+                canPurchase: true,
+                addToCart: false,
+                image: {src: './images/doona.jpg'}
+            },
+            {
+                name: 'formula',
+                price: '15',
+                showD: false,
+                description: 'best formula your baby will love it',
+                stock: '4',
+                soldOut: false,
+                canPurchase: true,
+                addToCart: false,
+                image: {src: './images/formula.jpg'}
+            },
+            {
+                name: 'bath',
+                price: '20',
+                showD: false,
+                description: 'must have',
+                stock: '0',
+                soldOut: true,
+                canPurchase: true,
+                addToCart: false,
+                image: {src: './images/bath.jpg'}
+            },
+            {
+                name: 'carrier',
+                price: '100',
+                showD: false,
+                description: 'an amazing package of diapers for your baby',
+                stock: '3',
+                soldOut: false,
+                canPurchase: true,
+                addToCart: false,
+                image: {src: './images/baby-carrier.jpg'}
+            },
+            {
+                name: 'crib',
+                price: '650',
+                showD: false,
+                description: 'bottle that your baby will love',
+                stock: '3',
+                soldOut: false,
+                canPurchase: true,
+                addToCart: false,
+                image: {src: './images/crib.jpg'}
+            },
+            {
+                name: 'diaper-bag',
+                price: '40',
+                showD: false,
+                description: 'must have',
+                stock: '0',
+                soldOut: true,
+                canPurchase: true,
+                addToCart: false,
+                image: {src: './images/diaper-bag.jpg'}
+            }
+        ];
+       $scope.selectedProduct=[{
+           name: 'diaper-bag',
+           price: '40',
+           showD: false,
+           color: "availablie in pink or blue",
+           description: 'must have bag.Very practical,easy to carry, and can contain all you need for your precious one',
+           releaseDate:"02/09/2016",
+           stock: '0',
+           soldOut: true,
+           canPurchase: true,
+           addToCart: false,
+           image: {src: '../images/diaper-bag.jpg'}
+       }];
+       $scope.showMe = false;
+
+       $scope.showDescription = function($event){
+           $scope.showMe = !$scope.showMe;
+           console.log($event.target);
+           
+       };
+
+
+            
+
     });
-}) ();
+})();
