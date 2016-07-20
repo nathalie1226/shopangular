c874c029f8a46b6b80c9c65a81089906af105d3f
nathalie <nathalie1226@hotmail.com>
2016-07-20 08:32:53 +0300

    routes working

--- shop/app.js
+++ shop/app.js
@@ -1,25 +1,25 @@
 
-(function () {
-
-    'use strict';
-    angular.module('myApp',[]);
-    
+// (function () {
+//
+//     'use strict';
+//     angular.module('myApp',[]);
+//    
         // .config(function ($routeProvider) {
-        //     $routeProvider
-        //         .when('/', {
-        //             templateUrl: 'shop.html',
-        //             controller: 'StoreController'
-        //         })
-        //         .when('/description', {
-        //             templateUrl: 'partials/description.html',
-        //             controller: 'StoreController'
-        //         })
-        //         .when('/cart', {
-        //             templateUrl: 'partials/cart.html',
-        //             controller: 'StoreController'
-        //         })
-        //         .otherwise({redirectTo: '/'});
-        // });
-
-
-})();
+    //     $routeProvider
+    //         .when('/', {
+    //             templateUrl: 'shop.html',
+    //             controller: 'StoreController'
+    //         })
+    //         .when('/description', {
+    //             templateUrl: 'partials/description.html',
+    //             controller: 'StoreController'
+    //         })
+    //         .when('/cart', {
+    //             templateUrl: 'partials/cart.html',
+    //             controller: 'StoreController'
+    //         })
+//     .otherwise({redirectTo: '/'});
+// });
+//
+//
+// })();
