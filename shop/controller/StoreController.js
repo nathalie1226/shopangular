8702dd34a1da083657f2f62f259e2f59f675f53d
nathalie <nathalie1226@hotmail.com>
2016-07-22 12:13:10 +0300

    pictures working little bug in the amount of items

--- shop/controller/StoreController.js
+++ shop/controller/StoreController.js
@@ -20,13 +20,16 @@
         if (localStorage.getItem("productsBought")) {
             $scope.productsBought = JSON.parse(localStorage.getItem("productsBought"));
             $scope.total = parseInt(localStorage.getItem("total"));
+            $scope.itemsBought = parseInt(localStorage.getItem("itemsBought"));
+
 
         }
 
 
         $http({
             method: 'GET',
-            url: 'https://api.myjson.com/bins/3x9d9'
+            // url: 'https://api.myjson.com/bins/3x9d9'
+            url:'https://api.myjson.com/bins/3itdt'
         }).then(function successCallback(response) {
             $scope.products = response.data;
             if ($routeParams.id) {
@@ -40,26 +43,28 @@
         });
 
 
-        $scope.itemsBought = 0;
 
+        $scope.itemsBought = 0;
 
         $scope.removeAllProducts = function () {
             localStorage.removeItem("productsBought");
-            $window.location.reload();
+            localStorage.removeItem('total');
+            // localStorage.removeItem('itemsBought');
+
         };
         $scope.addProduct = function (product) {
-            $scope.itemInCart = false;
+            $scope.itemInCart = false;document.getElementById("level");
 
             if (typeof $scope.productsBought == "undefined") {
                 $scope.productsBought = [];
 
             }
-            $scope.total=0;
+
+            $scope.total = 0;
             for (var i = 0; i < $scope.productsBought.length; i++) {
 
                 if ($scope.productsBought[i].name === product.name) {
                     $scope.productsBought[i].quantity++;
-                    $scope.itemsBought++;
                     $scope.itemInCart = true;
                 }
 
@@ -68,24 +73,44 @@
             if (!$scope.itemInCart) {
                 $scope.productsBought.push({
                     name: product.name, price: product.price,
-                    description: product.description, quantity: 1, image: {src: 'product.image.src'}
+                    description: product.description, quantity: 1, image:product.image
                 });
 
             }
+            $scope.itemsBought++;
+            console.log( $scope.itemsBought);
             for (var j = 0; j < $scope.productsBought.length; j++) {
                 $scope.total += $scope.productsBought[j].quantity * $scope.productsBought[j].price;
 
             }
-            localStorage.removeItem("productsBought");
             localStorage.setItem("productsBought", JSON.stringify($scope.productsBought));
-            localStorage.removeItem("total");
             localStorage.setItem("total", JSON.stringify($scope.total));
+            localStorage.setItem("itemsBought", JSON.stringify($scope.itemsBought));
+            console.log("after the JSON"+$scope.itemsBought);
 
 
         };
 
+        $scope.removeItem = function (index) {
+            $scope.itemsBought = parseInt(localStorage.getItem("itemsBought"));
+            $scope.total -= $scope.productsBought[index].price;
+            if ($scope.productsBought[index].quantity > 1) {
+                $scope.productsBought[index].quantity--;
+            }
+            else {
+                $scope.productsBought.splice(index, 1);
+                console.log("in" + index);
+                console.log("in again" + $scope.productsBought);
+            }
+            $scope.itemsBought--;
+            console.log($scope.itemsBought);
+            localStorage.setItem("productsBought", JSON.stringify($scope.productsBought));
+            localStorage.setItem("total", JSON.stringify($scope.total));
+            localStorage.setItem("itemsBought", JSON.stringify($scope.itemsBought));
+            console.log($scope.itemsBought);
+        };
 
-        
+
     }
 
 })();
